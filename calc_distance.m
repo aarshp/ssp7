0841df8f3473cdee2879b73ff6cf7523b0e53b34
Aarsh Agarwal <19aarsh@gmail.com>
2018-11-16 02:19:09 +0530

    updated comments

--- calc_distance.m
+++ calc_distance.m
@@ -1,3 +1,5 @@
+%Function that takes two matrices as input and returns
+% the maximum of the absolute value of the difference between the two.
 function d = calc_distance(mu,mu1)
 
 d = max(max(abs(mu-mu1))');
