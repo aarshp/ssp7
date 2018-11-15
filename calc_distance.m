d8a0a46efdfd103e566c2f8c71ffb4af94cedf5e
shubhke <shubhke@iitk.ac.in>
2018-11-16 00:08:28 +0530

    calc distance

--- calc_distance.m
+++ calc_distance.m
@@ -1,15 +1,5 @@
-function d = calc_distance(phi,phi1,J,K)
+function d = calc_distance(mu,mu1)
 
-d = 0;
-% d = max(max(abs(mu-mu1))');
-
-for jj = 1:J
-    for k = 1:K
-        dd = norm(reshape(phi(jj,k,:,:),[2,2])-reshape(phi1(jj,k,:,:),[2,2]));
-        if dd > d
-            d = dd;
-        end
-    end
-end
+d = max(max(abs(mu-mu1))');
 
 end
